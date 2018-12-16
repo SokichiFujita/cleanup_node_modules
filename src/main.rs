3917cbb2313b187979a5428def66b17f6c8682e4
SokichiFujita <sokichi.fujita@gmail.com>
2018-12-16 16:30:30 +0900

    display file list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let dir = "./";
+    let mut paths = fs::read_dir(dir).unwrap();
+
+    println!("Display files of {}", dir);
+
+    loop {
+        match paths.next() {
+            Some(path) => {
+                println!("{:?}", path.unwrap().path());
+            },
+            None => { break }
+        }
+    }
 }
